581f766cd506cd5924a0f4e52472f7d0d59a2b75
austin-kim <austin.kim@balancehero.cc>
2026-04-20 15:00:51 +0900

    fix: replace dead hits.seeyoufarm.com with komarev.com counter
    seeyoufarm service is down, switched to komarev.com/ghpvc

--- src/components/VisitorCount.tsx
+++ src/components/VisitorCount.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from "react";
 
-const COUNT_API =
-  "https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Faustin-personal.github.io%2Faustin-blog&count_bg=%23555555&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=true";
+const BADGE_URL =
+  "https://komarev.com/ghpvc/?username=austin-personal-blog&color=grey&style=flat-square&label=visitors";
 
 export default function VisitorCount() {
   const [loaded, setLoaded] = useState(false);
@@ -18,9 +18,9 @@ export default function VisitorCount() {
     <div className="mt-3">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src={COUNT_API}
+        src={BADGE_URL}
         alt="visitor count"
-        className="h-6"
+        className="h-5"
       />
     </div>
   );
